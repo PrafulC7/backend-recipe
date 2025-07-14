863e01ed0485e57168cf39d38d1e8c030e126cd6
PrafulC7 <prafullacharde00715@gmail.com>
2025-07-14 12:59:14 +0530

    chore: deleted books and added recipe model

--- index.js
+++ index.js
@@ -4,197 +4,182 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 const {initializeDatabase} = require("./db/db.connect")
-const Books = require("./models/books.models")
+const Recipe = require("./models/recipe.models")
 initializeDatabase();
 
-async function seedData(newBook){
-  try{
-const book = new Books(newBook)
-const savedBook = await book.save()
-return savedBook;
-  }catch(error){
-    throw error
-  }
+async function seedRecipe(newRecipe){
+try{
+const recipe = new Recipe(newRecipe)
+const savedRecipe = await recipe.save()
+return savedRecipe;
+}catch(err){
+  throw err;
+}
 }
 
-app.post("/books", async (req,res)=>{
-  try{
-const book = await seedData(req.body)
-res.status(201).json({message:"book added successfully", book:book})
-  }catch(err){
-res.status(500).json({err:"failed to add book"})
-  }
-})
-
-async function getAllBooks(){
-  try{
-const allBooks = await Books.find()
-return allBooks;
-  }catch(err){
-    throw err
-  }
-}
-
-app.get("/books", async (req,res)=>{
-  try{
-const books = await getAllBooks()
-res.send(books)
-  }catch(err){
-res.status(404).json({error:"books not found"})
-  }
-})
-
-async function getBookByTitle(bookTitle){
-  try{
-const book = await Books.find({title:bookTitle})
-return book
-  }catch(err){
-    throw err
-  }
-}
-
-app.get("/books/:title", async (req,res)=>{
-  try{
-const bookDetails = await getBookByTitle(req.params.title)
-if(bookDetails){
-  res.send(bookDetails)
+app.post("/recipe", async (req,res)=>{
+try{
+const recipeDetails = await seedRecipe(req.body)
+if(recipeDetails){
+  res.status(201).json({message:"Recipe added successfully", recipe:recipeDetails})
 }else{
-  res.status(404).json({err:"book not found"})
+  res.status(400).json({err:"Couldn't add recipe"})
+}
+}catch(err){
+  res.status(500).json({err:"Failed to fetch recipe"})
+}
+})
+
+async function getRecipe(){
+  try{
+    const recipe = Recipe.find()
+  return recipe
+  }catch(err){
+  throw err
+}
+}
+
+app.get("/recipe", async (req,res)=>{
+  try{
+const recipeDetails = await getRecipe()
+if(recipeDetails){
+  res.send(recipeDetails)
+}else{
+  res.status(404).json({err:"recipe not found"})
 }
   }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
+    res.status(500).json({err:"failed to fetch recipe"})
   }
 })
 
-async function getBookByAuthor(bookAuthor){
+async function getRecipeByTitle(recipeTitle){
   try{
-const book = await Books.find({author:bookAuthor})
-return book
+const recipe = await Recipe.find({title:recipeTitle})
+return recipe
+  }catch(err){
+    throw err
+  }
+}
+app.get("/recipe/:title", async (req,res)=>{
+  try{
+const recipeDetails = await getRecipeByTitle(req.params.title)
+if(recipeDetails){
+  res.send(recipeDetails)
+}else{
+  res.status(404).json({err:"recipe not found"})
+}
+  }catch(err){
+    res.status(500).json({err:"failed to fetch recipe"})
+  }
+})
+
+async function getRecipeByAuthor(recipeAuthor){
+  try{
+const recipe = await Recipe.find({author:recipeAuthor})
+return recipe
   }catch(err){
     throw err
   }
 }
 
-app.get("/books/author/:bookAuthor", async (req,res)=>{
+app.get("/recipe/author/:recipeAuthor", async (req,res)=>{
   try{
-const bookDetails = await getBookByAuthor(req.params.bookAuthor)
-if(bookDetails){
-  res.send(bookDetails)
+const recipeDetails = await getRecipeByAuthor(req.params.recipeAuthor)
+if(recipeDetails){
+  res.send(recipeDetails)
 }else{
-  res.status(404).json({err:"book not found"})
+  res.status(404).json({err:"recipe not found"})
 }
   }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
+    res.status(500).json({err:"failed to fetch recipe"})
   }
 })
 
-async function getBookByGenre(bookGenre){
+async function getRecipeByDifficulty(recipeDifficulty){
   try{
-const book = await Books.find({genre:bookGenre})
-return book
+const recipe = await Recipe.find({difficulty:recipeDifficulty})
+return recipe
   }catch(err){
     throw err
   }
 }
 
-app.get("/books/genre/:bookGenre", async (req,res)=>{
+app.get("/recipe/difficulty/:recipeDifficulty", async (req,res)=>{
   try{
-const bookDetails = await getBookByGenre(req.params.bookGenre)
-if(bookDetails){
-  res.send(bookDetails)
+const recipeDetails = await getRecipeByDifficulty(req.params.recipeDifficulty)
+if(recipeDetails){
+  res.send(recipeDetails)
 }else{
-  res.status(404).json({err:"book not found"})
+  res.status(404).json({err:"recipe not found"})
 }
   }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
+    res.status(500).json({err:"failed to fetch recipe"})
   }
 })
 
-async function getBookByPublishedYear(bookPublishedYear){
+async function updateById(recipeId, dataToUpdate){
   try{
-const book = await Books.find({publishedYear:bookPublishedYear})
-return book
+const recipe = await Recipe.findByIdAndUpdate(recipeId, dataToUpdate, {new:true})
+return recipe
   }catch(err){
     throw err
   }
 }
 
-app.get("/books/publishedYear/:bookPublishedYear", async (req,res)=>{
+app.post("/recipe/:recipeId", async (req,res)=>{
   try{
-const bookDetails = await getBookByPublishedYear(req.params.bookPublishedYear)
-if(bookDetails){
-  res.send(bookDetails)
+const recipeDetails = await updateById(req.params.recipeId, req.body)
+if(recipeDetails){
+  res.status(201).json({message:"recipe updated successfully.", recipe:recipeDetails})
 }else{
-  res.status(404).json({err:"book not found"})
+  res.status(404).json({err:"recipe not found"})
 }
   }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
+    res.status(500).json({err:"failed to fetch recipe"})
   }
 })
 
-async function updateById(bookId, dataToUpdate){
+async function updateByTitle(recipeTitle, dataToUpdate){
   try{
-const book = await Books.findByIdAndUpdate(bookId, dataToUpdate, {new:true})
-return book
+const recipe = await Recipe.findOneAndUpdate({title:recipeTitle}, dataToUpdate, {new:true})
+return recipe
   }catch(err){
     throw err
   }
 }
 
-app.post("/books/:bookId", async (req,res)=>{
+app.post("/recipe/title/:recipeTitle", async (req,res)=>{
   try{
-const bookDetails = await updateById(req.params.bookId, req.body)
-if(bookDetails){
-  res.status(201).json({message:"book updated successfully.", book:bookDetails})
+const recipeDetails = await updateByTitle(req.params.recipeTitle, req.body)
+if(recipeDetails){
+  res.status(201).json({message:"recipe updated successfully.", recipe:bookDetails})
 }else{
-  res.status(404).json({err:"book not found"})
+  res.status(404).json({err:"recipe not found"})
 }
   }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
+    res.status(500).json({err:"failed to fetch recipe"})
   }
 })
 
-async function updateByTitle(bookTitle, dataToUpdate){
+async function deleteById(recipeId){
   try{
-const book = await Books.findOneAndUpdate({title:bookTitle}, dataToUpdate, {new:true})
-return book
+const recipe = await Recipe.findByIdAndDelete(recipeId)
+return recipe
   }catch(err){
     throw err
   }
 }
 
-app.post("/books/title/:bookTitle", async (req,res)=>{
+app.delete("/recipe/:recipeId", async (req,res)=>{
   try{
-const bookDetails = await updateByTitle(req.params.bookTitle, req.body)
-if(bookDetails){
-  res.status(201).json({message:"book updated successfully.", book:bookDetails})
+const recipeDetails = await deleteById(req.params.recipeId)
+if(recipeDetails){
+  res.status(201).json({message:"This recipe deleted successfully.", recipe:recipeDetails})
 }else{
-  res.status(404).json({err:"book not found"})
+  res.status(404).json({err:"recipe not found"})
 }
   }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
-  }
-})
-
-async function deleteById(bookId){
-  try{
-const book = await Books.findByIdAndDelete(bookId)
-return book
-  }catch(err){
-    throw err
-  }
-}
-
-app.delete("/books/:bookId", async (req,res)=>{
-  try{
-const bookDetails = await deleteById(req.params.bookId)
-if(bookDetails){
-  res.status(201).json({message:"This book deleted successfully.", book:bookDetails})
-}else{
-  res.status(404).json({err:"book not found"})
-}
-  }catch(err){
-    res.status(500).json({err:"failed to fetch book"})
+    res.status(500).json({err:"failed to fetch recipe"})
   }
 })
 
